81639580001c1f25a10145a9c846c99add00ecc4
scristhian69 <quispeninacristhianlucio>
2023-05-12 11:39:33 -0400

    primer commit

--- src/App.js
+++ src/App.js
@@ -3,23 +3,21 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <container/>
+    </>
   );
 }
 
+
+
+function Container(props){
+  return(
+    <div>
+      {props.children}
+    </div>
+  );
+}
 export default App;
+
+
